44439c0dbb0f3cb21df2b5221ce0383a1a3905ab
Angelo Fausti <afausti@lsst.org>
2019-09-16 10:48:16 -0700

    Bump version: 0.1.2 → 0.2.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lsst-sqre/kafka-connect-manager',
-    version='0.1.2',
+    version='0.2.0',
     entry_points={
         'console_scripts': ['connect_manager = connect_manager.main:main']
     }
