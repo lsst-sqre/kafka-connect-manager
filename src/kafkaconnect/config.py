8dfdc1e1ba1112fdf8a946a6fbd7141407b84a7e
Angelo Fausti <afausti@lsst.org>
2020-07-24 06:14:33 -0700

    Add ConnectConfig data class
    - ConnectConfig keep the generic connector configuration

--- src/kafkaconnect/config.py
+++ src/kafkaconnect/config.py
@@ -1,15 +1,16 @@
-"""Configuration definition."""
+"""Kafkaconnect application and generic connector configuration."""
 
-__all__ = ["Configuration"]
+__all__ = ["Config", "ConnectConfig"]
 
+import json
 import os
-from dataclasses import dataclass, field
-from typing import List
+from dataclasses import asdict, dataclass
+from typing import Any, Dict, List, Tuple
 
 
 @dataclass
-class Configuration:
-    """Configuration for kafkaconnect."""
+class Config:
+    """Kafkaconnect application configuration."""
 
     broker_url: str = os.getenv("KAFKA_BROKER_URL", "localhost:9092")
     """The Kafka Broker URL.
@@ -21,33 +22,72 @@ class Configuration:
     The Kafka Connect REST API is used to manage connectors.
     """
 
-    topic_regex: str = os.getenv("TOPIC_REGEX", ".*")
+    topic_regex: str = os.getenv("KAFKA_CONNECT_TOPIC_REGEX", ".*")
     """Regex to select topics from Kafka."""
 
-    excluded_topics: List[str] = field(default_factory=list)
-    """List of topics excluded from Kafka."""
+    excluded_topics: str = os.getenv("KAFKA_CONNECT_EXCLUDED_TOPICS", "")
+    """Comma separated list of 'problematic' topics to exclude from
+    selection.
+    """
 
-    def __post_init__(self) -> None:
-        """Post config initialization steps."""
-        # Set default value for excluded_topics
-        self.excluded_topics = self._strtolist(
-            os.getenv("EXCLUDED_TOPICS", "")
-        )
+    check_interval: str = os.getenv("KAFKA_CONNECT_CHECK_INTERVAL", "15000")
+    """The interval, in milliseconds, to update the connector.
 
-    def _strtolist(self, s: str) -> List[str]:
-        """Convert comma separated values to a list of strings.
+    Check Kafka for new topics and update the connector configuration.
+    """
+
+
+@dataclass
+class ConnectConfig:
+    """Generic connector configuration."""
+
+    name: str = ""
+    """Name of the connector.
+
+    The connector name must be unique accross the cluster.
+    """
+
+    connector_class: str = ""
+    """Name of the connector class."""
+
+    topics: str = ""
+    """Comma separated list of Kafka topics to read from (sink connectors) or
+    to write to (source connectors).
+    """
+
+    tasks_max: int = int(os.getenv("KAFKA_CONNECT_TASKS_MAX", "1"))
+    """Number of connect tasks to scale out.
+
+    Topics and partitions are distributed accross tasks.
+    """
+
+    def update_topics(self, topics: List[str]) -> None:
+        """Update the list of Kafka topics.
 
         Parameters
         ----------
-        s : `str`
-            Comma separated values
-
-        Returns
-        -------
-        slist : `list`
+        topics : `list`
+            List of kafka topics.
         """
-        slist = s.replace(" ", "").split(",")
-        return slist
+        # Ensure uniqueness and sort topic names
+        topics = list(set(topics))
+        topics.sort()
+        self.topics = ",".join(topics)
 
+    @staticmethod
+    def format_field_names(fields: List[Tuple[str, Any]]) -> Dict[str, str]:
+        """Dictionary factory to use with the dataclasses.asdict() method.
 
-config = Configuration()
+        Rename the field name replacing '_' by '.' and return a dictionary
+        mapping field names to field values.
+        """
+        result = []
+        for f in fields:
+            name, value = f
+            name = name.replace("_", ".")
+            result.append((name, value))
+        return dict(result)
+
+    def asjson(self) -> str:
+        config = asdict(self, dict_factory=self.format_field_names)
+        return json.dumps(config, indent=4, sort_keys=True)
