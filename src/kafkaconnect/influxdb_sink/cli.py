d6213976553b872027516137ae99f01fef7b5379
Angelo Fausti <angelofausti@gmail.com>
2021-06-02 15:36:20 -0700

    Use excluded_topic_regex in the InfluxDB connector

--- src/kafkaconnect/influxdb_sink/cli.py
+++ src/kafkaconnect/influxdb_sink/cli.py
@@ -141,15 +141,14 @@ from kafkaconnect.topics import Topic
 )
 @click.option(
     "-e",
-    "--excluded_topics",
-    "excluded_topics",
-    envvar="KAFKA_CONNECT_EXCLUDED_TOPICS",
+    "--excluded_topic_regex",
+    "excluded_topic_regex",
+    envvar="KAFKA_CONNECT_EXCLUDED_TOPIC_REGEX",
     default="",
     show_default=True,
     help=(
-        "Comma separated list of topics to exclude from "
-        "selection. Alternatively set via the "
-        "$KAFKA_CONNECT_EXCLUDED_TOPICS env var."
+        "Regex for excluding topics. Alternatively set via the "
+        "$KAFKA_CONNECT_EXCLUDED_TOPIC_REGEX env var."
     ),
 )
 @click.option(
@@ -227,7 +226,7 @@ def create_influxdb_sink(
     auto_update: bool,
     validate: bool,
     check_interval: str,
-    excluded_topics: str,
+    excluded_topic_regex: str,
     connect_influx_error_policy: str,
     connect_influx_max_retries: str,
     connect_influx_retry_interval: str,
@@ -263,7 +262,9 @@ def create_influxdb_sink(
     topics: List[str] = list(topiclist)
     if not topics:
         click.echo("Discoverying Kafka topics...")
-        topics = Topic(config.broker_url, topic_regex, excluded_topics).names
+        topics = Topic(
+            config.broker_url, topic_regex, excluded_topic_regex
+        ).names
         n = 0 if not topics else len(topics)
         click.echo(f"Found {n} topics.")
     connect = Connect(connect_url=config.connect_url)
@@ -309,7 +310,7 @@ def create_influxdb_sink(
             try:
                 # Current list of topics from Kafka
                 current_topics = Topic(
-                    config.broker_url, topic_regex, excluded_topics
+                    config.broker_url, topic_regex, excluded_topic_regex
                 ).names
                 new_topics = list(set(current_topics) - set(topics))
                 if new_topics:
